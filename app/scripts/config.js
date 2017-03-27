d15759d6bd2e9bce028b41d59fdbc796d83b4fa9
sjpl85 <samuel.jesus.patino.lopez@gmail.com>
2017-03-27 19:25:36 +0200

    Update config.js

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,11 +1,9 @@
 angular.module('appConfig', [])
     .constant('mainConfig', {
-        'region': 'eu-west-2',
-        // 'accessKeyId': 'AKIAJSCCYI62K3R3GURQ',
-        // 'secretAccessKey': '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',
+        'region': '',
         'refreshTime': 5000,
-        'imageId': 'ami-665b4f02',
+        'imageId': '',
         'instanceType': 't2.micro',
-        'securityGroupIds': ['sg-8f71c7e6'],
+        'securityGroupIds': [''],
         'keyName': undefined
     });
